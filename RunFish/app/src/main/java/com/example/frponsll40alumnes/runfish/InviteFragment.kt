2e08ac7561e88541c8413e25895558bb8ada7949
bernatosoriogomez <bernatosoriogomez@users.noreply.github.com>
2019-03-23 16:49:49 +0100

    INVITE DIALOGFRAG A MITJES

--- RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/InviteFragment.kt
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/InviteFragment.kt
@@ -6,19 +6,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.support.v4.app.DialogFragment
 import kotlinx.android.synthetic.main.fragment_invite.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
-class InviteFragment : Fragment() {
+class InviteFragment : DialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
