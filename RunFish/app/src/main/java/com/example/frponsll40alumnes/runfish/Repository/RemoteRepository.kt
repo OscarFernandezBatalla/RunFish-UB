763aa8ca21745e560b3fb12e7967c9f413a6b48e
bernatosoriogomez <bernatosoriogomez@users.noreply.github.com>
2019-04-04 20:25:16 +0200

    Repository in progress
    Co-Authored-By: oriolornaque <oriolornaque@users.noreply.github.com>

---
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/Repository/RemoteRepository.kt
@@ -0,0 +1,14 @@
+package com.example.frponsll40alumnes.runfish.Repository
+
+class RemoteRepository: DataModel{
+    /*
+    TODO: Add methods:
+
+        getFish() -> Returns array of fish that are playing
+        getBombs() -> Returns all bombs created in map.
+        getPlankton() -> Returns array of all plankton created in map.
+        getSharks()  -> Returns array of all sharks created in map.
+        getMap() -> Calls other functions.
+        and setters.
+     */
+}
---
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/Repository/Repository.kt
@@ -0,0 +1,6 @@
+package com.example.frponsll40alumnes.runfish.Repository
+
+class Repository{
+
+    //TODO: Will include LocalRepository object and RemoteRepository object.
+}
