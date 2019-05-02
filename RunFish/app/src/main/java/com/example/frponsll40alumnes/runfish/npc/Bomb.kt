3e65aac979fb0fbb45fdcc0d41e4eede97206099
bernatosoriogomez <bernatosoriogomez@users.noreply.github.com>
2019-05-02 15:04:55 +0200

    Update Bomb.kt

--- RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/npc/Bomb.kt
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/npc/Bomb.kt
@@ -1,8 +1,45 @@
 package com.example.frponsll40alumnes.runfish.npc
 
-class Bomb: NPC(value = 25) {
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+
+class Bomb(var image: Bitmap): NPC(value = 25) {
     override fun changePosition() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    private var xVelocity = 20
+    private var yVelocity = 20
+    private val screenWidth = Resources.getSystem().displayMetrics.widthPixels
+    private val screenHeight = Resources.getSystem().displayMetrics.heightPixels
+    
+
+    init {
+
+        var height = image.height
+        var width = image.width
+
+        x = screenWidth/2 //random positions
+        y = -screenHeight
+    }
+
+    /**
+     * Draws the object on to the canvas.
+     */
+    fun draw(canvas: Canvas) {
+        canvas.drawBitmap(image, x.toFloat(), y.toFloat(), null)
+    }
+
+    /**
+     * update properties for the game object
+     */
+    fun update() {
+
+        y+= (yVelocity)
+
+
     }
 
 }
