55c5f599c1f1c5ebab63ed1e77df4c8143822f74
oriolOrnaque <32044766+oriolornaque@users.noreply.github.com>
2019-05-20 22:05:36 +0200

    bite ability implemented

--- RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/abilities/Bite.kt
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/abilities/Bite.kt
@@ -1,27 +1,17 @@
 package com.example.frponsll40alumnes.runfish.abilities
 
 import com.example.frponsll40alumnes.runfish.fish.Fish
-import com.example.frponsll40alumnes.runfish.MVP.Presenter
+import com.example.frponsll40alumnes.runfish.fish.Shark
 
 class Bite : AbilityStrategy {
 
 
     override fun useAbility(fish: Fish) {
-        /* the range of the ability */
-        val y_range = 100
-        val x_radius = 20
-        /* check if there's some npc in range */
-        /* eliminate npc */
 
-        /*
-        for(npc in presenter.getNPCsInArea( fish.x - x_radius,
-                                            fish.x + x_radius,
-                                            fish.y,
-                                            fish.y + y_range)){
-            npc.die()
+        if(fish is Shark) {
+            fish.biteActivated = true;
+            fish.biteFrame = 1;
         }
-        */
 
-        TODO("Com podem accedir a GameEngine?") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/fish/Shark.kt
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/fish/Shark.kt
@@ -5,12 +5,19 @@ import android.graphics.*
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.RectShape
 import com.example.frponsll40alumnes.runfish.R
+import com.example.frponsll40alumnes.runfish.npc.NPC
+import com.example.frponsll40alumnes.runfish.npc.Plankton
 
 class Shark(context: Context) : Fish("Shark",20,0,"bite",1000,20,20){
     override var image = BitmapFactory.decodeResource(context.resources, R.drawable.shark)
     override val width: Int = image.width
     override val height: Int = image.height
     override var rectangle: Rect = Rect(this.x, this.y, this.x+width, this.y+height)
+
+    var biteFrame = 0;
+    var biteActivated = false;
+    var collisionScaled = false;
+    val biteLength = 100;
 
     init{
         //test de col·lisions (temporal):
@@ -19,6 +26,38 @@ class Shark(context: Context) : Fish("Shark",20,0,"bite",1000,20,20){
         rec.paint.color= Color.TRANSPARENT
         rec.paint.style= Paint.Style.STROKE
         rec.paint.color = Color.GREEN
+    }
+
+    override fun update(xJoy : Double, yJoy : Double, strength : Int) {
+        super.update(xJoy, yJoy, strength)
+        if(this.biteFrame > 0){
+            this.biteFrame--
+        }else{
+            if(this.biteActivated)
+                this.biteActivated = false;
+        }
+        if(this.biteFrame <= 0 && this.biteActivated && this.collisionScaled){
+            this.rectangle = Rect(this.x, this.y, this.x+width, this.y+height)
+            this.collisionScaled = false;
+        }else if(this.biteFrame >= 0 && this.biteActivated && !(this.collisionScaled)){
+            this.rectangle = Rect(this.x, this.y, this.x+width, this.y+height+this.biteLength)
+            this.collisionScaled = true;
+        }
+    }
+
+    override fun collision(npc: NPC) {
+        if(npc is Plankton){
+            this.gainCapacity(npc.value)
+        }
+        else {
+            // Si CommonFish no té activat el shield per aquest frame
+            if(this.biteActivated){
+                npc.die()
+                // emite vibration in loseLife
+            }else{
+                this.loseLife(npc.value)
+            }
+        }
     }
 }
 
