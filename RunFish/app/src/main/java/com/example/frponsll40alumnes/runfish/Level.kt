d1c48452e299a0a047cb692afd86f2102e8f8fd5
bernatosoriogomez <bernatosoriogomez@users.noreply.github.com>
2019-06-01 14:31:09 +0200

    Revert "correció freemode"
    This reverts commit 72e5a2fc4273641c5992c0a4c3e33e61fa73dca2.

--- RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/Level.kt
+++ RunFish/app/src/main/java/com/example/frponsll40alumnes/runfish/Level.kt
@@ -28,38 +28,33 @@ open class Level (var numLevel : Int, val context : Context)
 
     init{
         setMap()
-        if (numLevel != -1){
-            for (i in 1..(-meters/1000)){
-                val leftCoral = BackgroundElement(BackgroundType.LEFT_CORAL, context)
-                val rightCoral = BackgroundElement(BackgroundType.RIGHT_CORAL, context)
-                val leftTurtle = BackgroundElement(BackgroundType.LEFT_TURTLE, context)
-                val rightTurtle = BackgroundElement(BackgroundType.RIGHT_TURTLE, context)
-                val leftFish = BackgroundElement(BackgroundType.LEFT_FISH, context)
-                val rightFish = BackgroundElement(BackgroundType.RIGHT_FISH, context)
+        for (i in 1..(-meters/1000)){
+            val leftCoral = BackgroundElement(BackgroundType.LEFT_CORAL, context)
+            val rightCoral = BackgroundElement(BackgroundType.RIGHT_CORAL, context)
+            val leftTurtle = BackgroundElement(BackgroundType.LEFT_TURTLE, context)
+            val rightTurtle = BackgroundElement(BackgroundType.RIGHT_TURTLE, context)
+            val leftFish = BackgroundElement(BackgroundType.LEFT_FISH, context)
+            val rightFish = BackgroundElement(BackgroundType.RIGHT_FISH, context)
 
-                leftCoral.setCoordenates(0, -i*1500)
-                rightCoral.setCoordenates(displayWidth+heightNavegationBar-rightCoral.getImageWidth(), -i*1000)
-                leftTurtle.setCoordenates(10, -i*1500)
-                rightTurtle.setCoordenates(displayWidth+heightNavegationBar-rightTurtle.getImageWidth(), -i*1500)
-                leftFish.setCoordenates(leftCoral.getImageWidth(), -i*1500)
-                rightFish.setCoordenates(displayWidth+heightNavegationBar-rightFish.getImageWidth(), -i*1500)
+            leftCoral.setCoordenates(0, -i*1500)
+            rightCoral.setCoordenates(displayWidth+heightNavegationBar-rightCoral.getImageWidth(), -i*1000)
+            leftTurtle.setCoordenates(10, -i*1500)
+            rightTurtle.setCoordenates(displayWidth+heightNavegationBar-rightTurtle.getImageWidth(), -i*1500)
+            leftFish.setCoordenates(leftCoral.getImageWidth(), -i*1500)
+            rightFish.setCoordenates(displayWidth+heightNavegationBar-rightFish.getImageWidth(), -i*1500)
 
-                this.elements.add(leftCoral)
-                this.elements.add(rightCoral)
-                this.elements.add(leftTurtle)
-                this.elements.add(rightTurtle)
-                this.elements.add(leftFish)
-                this.elements.add(rightFish)
-
-                val difficulty = difficultyFactory.createDifficulty(difficultyLevel!!)
-                npcs[NPCType.PLANKTON] = difficulty!!.getNPlankton()
-                npcs[NPCType.BOMB] = difficulty.getNBombs()
-                npcs[NPCType.ENEMYSHARK] = difficulty.getNSharks()
-            }
-        }else{
-
+            this.elements.add(leftCoral)
+            this.elements.add(rightCoral)
+            this.elements.add(leftTurtle)
+            this.elements.add(rightTurtle)
+            this.elements.add(leftFish)
+            this.elements.add(rightFish)
         }
 
+        val difficulty = difficultyFactory.createDifficulty(difficultyLevel!!)
+        npcs[NPCType.PLANKTON] = difficulty!!.getNPlankton()
+        npcs[NPCType.BOMB] = difficulty.getNBombs()
+        npcs[NPCType.ENEMYSHARK] = difficulty.getNSharks()
     }
 
     private fun setMap(){
